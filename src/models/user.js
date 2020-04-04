6cdd3f24e95cd69f2e51f9731068756e173676b2
barun1997 <barun.pandey@icloud.com>
2020-04-05 00:55:09 +0545

    initial boilerplate for my task app

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email must be valid");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.length <= 6) {
+        throw new Error("Password must be of length greater than 6");
+      } else if (value.includes("password")) {
+        throw new Error("Password cannot be set to password");
+      }
+    }
+  }
+});
+
+module.exports = User;
