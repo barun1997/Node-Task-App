f8efe32adf7c5990f17a91e55072aebe3d487182
barun1997 <barun.pandey@icloud.com>
2020-04-06 11:08:53 +0545

    sort, pagination included

--- src/index.js
+++ src/index.js
@@ -15,14 +15,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port" + port);
 });
-
-const User = require("./models/user");
-const main = async () => {
-  // const task = await Task.findById("5e89ffcf5635912d10a8ad51");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-  const user = await User.findById("5e8a03601631ec2f10f33d2d");
-  await user.populate("tasks").execPopulate();
-};
-
-main();
--- src/models/user.js
+++ src/models/user.js
@@ -4,54 +4,59 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Task = require("./tasks");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email must be valid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (value.length <= 6) {
-        throw new Error("Password must be of length greater than 6");
-      } else if (value.includes("password")) {
-        throw new Error("Password cannot be set to password");
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be a positive number");
+        }
       },
     },
-  ],
-});
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email must be valid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.length <= 6) {
+          throw new Error("Password must be of length greater than 6");
+        } else if (value.includes("password")) {
+          throw new Error("Password cannot be set to password");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.virtual("tasks", {
   ref: "Tasks",
